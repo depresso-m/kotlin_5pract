b20ca9e71e79c5464530250294229aece573741d
depresso-m <depressomisha@gmail.com>
2024-09-15 23:48:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kotlin_5pract/SecondFragment.kt
@@ -0,0 +1,38 @@
+package com.example.kotlin_5pract
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.kotlin_5pract.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+    private var _binding : FragmentSecondBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.to1fragmentBtn.setOnClickListener{
+            findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
+        }
+    }
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
